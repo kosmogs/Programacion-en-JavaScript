cade66d9a76cff88de76c94936b3106ffb182a9b
Juan J <jgomezquintero7@gmail.com>
2025-08-01 11:36:34 -0500

    Agregar funciones 1 y 2 sena proyecto

---
+++ Proyecto Sena/js/funcionalidades.js
@@ -0,0 +1,20 @@
+//OJITO PARA VER CONTRASEÑA
+
+/*<script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const passwordField = document.getElementById('passwordField');
+            const togglePassword = document.getElementById('togglePassword');
+
+            togglePassword.addEventListener('click', function() {
+                // Determina el nuevo tipo del input
+                const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+                passwordField.setAttribute('type', type);
+
+                // Cambia el icono del ojo
+                this.textContent = (type === 'password') ? '👁️' : '🙈';
+            });
+        });
+</script>*/
+
+//ALERTA CONDICIONAL CONTRASEÑA MAYUS, MINUS, NUMERO, CARACTER ESPECIAL
+
